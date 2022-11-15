0fe1066e5b0ab0f09a814e890ed90124c7dc4eba
Tiaraaaaaa <99133045+tiaraaqll@users.noreply.github.com>
2022-11-15 22:36:35 +0700

    Update

---
+++ Assets/Script/PhoneGravity.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneGravity : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float gravityMagnitude;
+    bool useGyro;
+    Vector3 gravityDir;
+
+    void Start() {
+        if (SystemInfo.supportsGyroscope) {
+            useGyro = true;
+            Input.gyro.enabled = true;
+        }
+    }
+
+    void Update() {
+        var InputDir = useGyro ? Input.gyro.gravity : Input.acceleration;
+        gravityDir = new Vector3(InputDir.x, InputDir.z, InputDir.y);
+    }
+
+    void FixedUpdate() {
+        rb.AddForce(gravityDir * gravityMagnitude, ForceMode.Acceleration);
+    }
+}
